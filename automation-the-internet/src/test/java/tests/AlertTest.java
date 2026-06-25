9012fd25a2140b836b6a7053da8e4caa64197313
shaima <shaima.anvar@gmail.com>
2026-06-24 22:24:08 -0400

    Updated JavaScript alert automation test code with comments

--- automation-the-internet/src/test/java/tests/AlertTest.java
+++ automation-the-internet/src/test/java/tests/AlertTest.java
@@ -7,14 +7,32 @@ import org.testng.annotations.Test;
 import pages.AlertPage;
 
 public class AlertTest {
-    @Test
-    public void jsAlertTest(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-        AlertPage alertPage = new AlertPage(driver);
-        alertPage.clickJsAlertButton();
-        alertPage.acceptAlert();
-        Assert.assertEquals(alertPage.getResultMessage(),"You successfully clicked an alert");
-    }
 
+    @Test
+    public void jsAlertTest() {
+
+        // Launch Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open JavaScript Alerts page
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+        // Create AlertPage object
+        AlertPage alertPage = new AlertPage(driver);
+
+        // Click "Click for JS Alert" button
+        alertPage.clickJsAlertButton();
+
+        // Accept the alert (click OK)
+        alertPage.acceptAlert();
+
+        // Verify success message
+        Assert.assertEquals(
+                alertPage.getResultMessage(),
+                "You successfully clicked an alert"
+        );
+
+        // Close browser
+        driver.quit();
+    }
 }
